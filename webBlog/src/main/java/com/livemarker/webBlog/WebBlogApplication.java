292ade6fc9058b04434043d9f93fd49838bcff0a
Elovenko Nikita <livemarker@users.noreply.github.com>
2021-03-05 13:10:04 +0300

    first settings

---
+++ webBlog/src/main/java/com/livemarker/webBlog/WebBlogApplication.java
@@ -0,0 +1,13 @@
+package com.livemarker.webBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBlogApplication.class, args);
+	}
+
+}
---
+++ webBlog/src/test/java/com/livemarker/webBlog/WebBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livemarker.webBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
