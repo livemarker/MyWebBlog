836b4452896afbaf498eb0020335df31d64f7e80
Elovenko Nikita <livemarker@users.noreply.github.com>
2021-03-09 13:30:24 +0300

    add web page for add text for blog

--- webBlog/src/main/java/com/livemarker/webBlog/controllers/BlogController.java
+++ webBlog/src/main/java/com/livemarker/webBlog/controllers/BlogController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
@@ -17,5 +19,16 @@ public class BlogController {
         Iterable<Post> posts=postRepository.findAll();
         model.addAttribute("posts",posts);
         return "blogMain";
+    }
+    @GetMapping("/blogMain/blogAdd")
+    public String blogAdd(Model model) {
+
+        return "blogAdd";
+    }
+    @PostMapping ("/blogMain/blogAdd")
+    public String blogPostAdd(@RequestParam String title,@RequestParam String announce,@RequestParam String fullText, Model model){
+        Post post=new Post(title,announce,fullText);
+        postRepository.save(post);
+        return "redirect:/blogMain";
     }
 }
